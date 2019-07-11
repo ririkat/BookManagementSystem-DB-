5c29d16638f518a66b8a4e1f7cc3f0d6f63d5241
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-07-11 19:28:51 +0900

    merge

--- src/application/kh/bms/controller/UserDeleteController.java
+++ src/application/kh/bms/controller/UserDeleteController.java
@@ -1,27 +1,24 @@
 package application.kh.bms.controller;
 
-import java.util.ArrayList;
-
-import application.kh.bms.model.dao.LoadSave;
+import application.kh.bms.model.service.RentalService;
 import application.kh.bms.model.service.UserService;
 import application.kh.bms.model.vo.User;
 
 public class UserDeleteController {
 	private UserService service = new UserService();
-
+//	private RentalService rentalService = new 
 	//ÇØ¾ßÇÔ
 	public int checkUser(String id, String pw) {
 		int check = -1;
-//		String nowUser = dao.getNowUser();
 //		User temp = service.oneUserSelect(id);
-//
+//		
 //		if (temp.getPw().equals(pw)) {
 //			if (users.get(i).getRetalList().isEmpty() == true) {
 //				check = i;
 //				break;
 //			}
 //		}
-//
+
 		return check;
 	}
 
